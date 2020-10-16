9d37fca724f039d28b50663c983a46e9288e065b
lhq3361951 <498276217@qq.com>
2020-10-16 15:02:42 +0800

    1st 2nd

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapp.activity.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+}

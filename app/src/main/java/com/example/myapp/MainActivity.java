9c7d63c963e7dc964cae9e99e08001f3124918d4
lhq3361951 <498276217@qq.com>
2020-10-21 00:42:02 +0800

    4tH

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.myapp.activity.BaseActivity;
 import com.example.myapp.activity.LoginActivity;
 import com.example.myapp.activity.RegisterActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private Button btnLogin;
     private Button btnRegister;
@@ -24,16 +25,14 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(in);
+                navigateTo(LoginActivity.class);
             }
         });
         btnRegister = findViewById(R.id.btn_register);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Re = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(Re);
+                navigateTo(RegisterActivity.class);
             }
         });
 
--- app/src/main/java/com/example/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapp/activity/LoginActivity.java
@@ -3,14 +3,44 @@ package com.example.myapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapp.R;
+import com.example.myapp.util.StringUtils;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    private EditText etAccount;
+    private EditText etPwd;
+    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etAccount = findViewById(R.id.etAccount);
+        etPwd = findViewById(R.id.etPwd);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = etAccount.getText().toString().trim();
+                String pwd = etPwd.getText().toString().trim();
+                login(account,pwd);
+            }
+        });
+    }
+    private void login(String account, String pwd){
+        if(StringUtils.isEmpty(account)){
+            showToast("请输入账号");
+            return;
+        }
+        if (StringUtils.isEmpty(pwd)){
+            showToast("请输入密码");
+        }
     }
 }
